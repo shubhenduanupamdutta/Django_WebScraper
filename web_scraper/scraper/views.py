8d3d99191ad4157e914ef13e20c85b425fc80e47
ShubhenduADutta <shubhendu@flexli.in>
2023-11-28 18:42:06 +0530

    Initial Setup
    Create a postgresql database connection
    Generated a new secret key
    created a scrape view

---
+++ web_scraper/scraper/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
+# Create your views here.
+
+
+def scrape(request):
+    response = requests.get('https://www.google.com/')
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    all_links = []
+    for link in soup.find_all('a'):
+        all_links.append(link.get('href'))
+
+    return render(request, 'scraper/scrape.html', {"links": all_links})
